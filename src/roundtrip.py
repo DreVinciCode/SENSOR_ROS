8f75df6a3eabdc3f0a729f4f51276b900bc7d7d6
DreVinciCode <dre.vivero@gmail.com>
2021-12-11 11:22:31 -0500

    Update roundtrip.py

--- src/roundtrip.py
+++ src/roundtrip.py
@@ -11,22 +11,30 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description="A script to command the Turtlebot2 to make a round trip on selected points on a map.")
-parser.add_argument('-n', metavar='NumPoints' , help="Specify the number of points for the path.", default='eth0')
+parser.add_argument('--n', metavar='NumPoints', type=int , help="Specify the number of points (2-10) for the robot to travel towards.")
 args=parser.parse_args()
 
-
 numPoints = 0
+minPoints = 2
+maxPoints = 10
 
-def Main():
-
-    if args.n > 1 :
-        try:
-            print("Selected %(filename)s..." % {"filename" : args.n})
-            
-        except:
-            print("Can\'t read PCAP file %(filename)s!" % {"filename" : args.r})
-    else
-
+def Main():            
+    userInput = 2
+    
+    try:
+        if args.n >= 2 :
+            print("Entered in %(value)s points." % {"value" : args.n})
+            userInput = args.n        
+    
+    except:
+        while True:
+            userInput = input("Enter number of points between 2 and 10: ")
+            try:
+                if(isinstance(int(userInput), int) and int(userInput) >= minPoints and int(userInput) < maxPoints):
+                    print("Entered in %(value)s points." % {"value" : userInput})
+                    break      
+            except:
+                continue
 
 
 if __name__ == "__main__":
