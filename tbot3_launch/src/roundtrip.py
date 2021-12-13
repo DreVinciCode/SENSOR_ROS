45995dd6d983db2fb8c03e6e5e3ce3b51e70a933
Andre Cleaver <andre.cleaver@tufts.edu>
2021-12-13 15:19:25 -0500

    changed file locations

--- tbot3_launch/src/roundtrip.py
+++ tbot3_launch/src/roundtrip.py
@@ -7,8 +7,13 @@ Andre Cleaver
 Tufts University
 '''
 
-import os
 import argparse
+import numpy as np
+import os
+import rospy
+
+from geometry_msgs.msg import Pose, Point, PointStamped
+
 
 parser = argparse.ArgumentParser(description="A script to command the Turtlebot2 to make a round trip on selected points on a map.")
 parser.add_argument('--n', metavar='NumPoints', type=int , help="Specify the number of points (2-10) for the robot to travel towards.")
@@ -18,7 +23,16 @@ numPoints = 0
 minPoints = 2
 maxPoints = 10
 
-def Main():            
+class GoToWayPoints:
+    def __init__(self):
+        rospy.Subscriber("/point_coordinate", Point, self.coordinate_callback)
+
+
+
+    def coordinate_callback(self, data):
+        print("coordinate_callback")
+
+def NumberOfWayPoints():            
     userInput = 2
     
     try:
@@ -32,10 +46,23 @@ def Main():
             try:
                 if(isinstance(int(userInput), int) and int(userInput) >= minPoints and int(userInput) < maxPoints):
                     print("Entered in %(value)s points." % {"value" : userInput})
-                    break      
+
+                    return int(userInput)     
             except:
                 continue
 
 
 if __name__ == "__main__":
-    Main()
+    
+
+    try:
+        test = NumberOfWayPoints()
+
+        # rospy.init_node("roundtrip", anonymous=False)
+
+        # WayPointFollower = GoToWayPoints()
+
+
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Quitting")
