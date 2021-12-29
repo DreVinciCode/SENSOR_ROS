bb02ba428ab148ad8f6ca619a33a7845511752fa
Andre Cleaver <andre.cleaver@tufts.edu>
2021-12-29 14:12:52 -0500

    transformed localization particles frame to baselink

--- tbot3_launch/src/sensar_ros_particlecloud.cpp
+++ tbot3_launch/src/sensar_ros_particlecloud.cpp
@@ -6,7 +6,11 @@ Tufts University
 *****************************/
 
 #include "geometry_msgs/PoseArray.h"
+#include "geometry_msgs/PoseStamped.h"
 #include "ros/ros.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_ros/transform_listener.h"
+#include <iostream>
 #include <sstream>
 #include <string>
 
@@ -19,13 +23,33 @@ using namespace std;
 
 const int FREQUENCY = 30;
 
+geometry_msgs::TransformStamped transformToBase_link;
 geometry_msgs::PoseArray latestMsg;
 geometry_msgs::PoseArray previousMsg;
 ros::Publisher particleCloudPub;
 
 geometry_msgs::PoseArray poseTransformation(geometry_msgs::PoseArray input)
 {
-    geometry_msgs::PoseArray transformed = input; 
+    geometry_msgs::PoseArray transformed;
+	int arraySize = input.poses.size(); 
+
+    std::vector<geometry_msgs::Pose> poses; 
+    
+    transformed.header = input.header;
+    transformed.header.frame_id = FRAME_OUT;
+    
+    
+	for (int i = 0; i < arraySize; i++) 
+	{
+		geometry_msgs::PoseStamped stampedPose;
+		stampedPose.header = input.header;
+	    stampedPose.pose = input.poses[i];
+    	tf2::doTransform(stampedPose, stampedPose, transformToBase_link);
+		poses.push_back(stampedPose.pose);	
+	}
+	
+	transformed.poses = poses;
+	
     return transformed;
 }
 
@@ -48,10 +72,22 @@ int main (int argc, char **argv)
     particleCloudPub = n.advertise<geometry_msgs::PoseArray>(TOPIC_OUT,5);
     ros::Subscriber globalSub  = n.subscribe(TOPIC_IN, 5, cloudCallback);
 
+    tf2_ros::Buffer tBuffer;
+    tf2_ros::TransformListener tf2_listener (tBuffer);
+
     ros::Rate rate(FREQUENCY);
 
     while(ros::ok())
     {
+		try
+        {
+            transformToBase_link = tBuffer.lookupTransform(FRAME_OUT, FRAME_IN, ros::Time(0), ros::Duration(1.0));
+        }
+        catch(tf2::TransformException e)
+        {
+            ROS_INFO("%s \n", e.what());
+        }
+        
         ros::spinOnce();    
         publishLatest();
         rate.sleep();
