f5abb2cfee57239f0fde4bdd9d4f4cfe81775fb5
DreVinciCode <andre.cleaver@tufts.edu>
2022-02-25 16:55:26 -0500

    added slider publisher

--- turtlebot/src/master_gui.py
+++ turtlebot/src/master_gui.py
@@ -2,7 +2,7 @@
 
 import rospy
 from std_msgs.msg import Bool
-
+from std_msgs.msg import Float64
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivy.lang import Builder
@@ -13,22 +13,20 @@ class KivyMD_GUI(MDApp):
 	
 	def __init_(self, **kwargs):
 		super().__init__(**kwargs)
-
-
-		
+	
 	def build(self):
 		self.screen=Builder.load_file('ros_gui.kv')
 		return self.screen
 
 	def inc_person_threshold(self, *args):
 
-		# msg=True
-		# pub.publish(msg)
 		if rospy.has_param('/SENSAR/person_threshold'):
 			threshold = rospy.get_param("/SENSAR/person_threshold")
 			threshold = round(threshold  + 0.1, 1)
 			rospy.set_param('/SENSAR/person_threshold', threshold)
 			print(threshold)
+
+			self.screen.ids.person_threshold_value_text.text = str(threshold)
 
 	def dec_person_threshold(self, *args):
 
@@ -38,10 +36,15 @@ class KivyMD_GUI(MDApp):
 			rospy.set_param('/SENSAR/person_threshold', threshold)
 			print(threshold)
 
+			self.screen.ids.person_threshold_value_text.text = str(threshold)
+
+	def slider_function(self, slider_value):
+		print(round(slider_value,0))
+		pub.publish(slider_value)
 
 if __name__ == "__main__":
 	
-	# pub = rospy.Publisher('/SENSAR/person_threshold', Bool, queue_size=2)
+	pub = rospy.Publisher('/SENSAR/slider_value', Float64, queue_size=2)
 
 	rospy.init_node('kivymd_gui', anonymous=True)	
 
