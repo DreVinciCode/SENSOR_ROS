5f0b1c944f39a3db1f4eb46af60ed6acc4721640
Andre Cleaver <andre.cleaver@tufts.edu>
2023-05-02 19:50:23 -0400

    updated navigation project

--- turtlebot/src/NavGoal.py
+++ turtlebot/src/NavGoal.py
@@ -17,10 +17,27 @@ from nav_msgs.msg import *
 from math import atan2
 from tf import transformations
 
+def generate_pointstamped():
+    return (Point(0.59, -0.57, 0.0), # A
+    		Point(0.62, 0.53, 0.0), # B
+    		Point(-1.14, 0.54, 0.0), # M
+ 			# set points from remapped room
+
+            # points are in map frame
+)
+
+
 class WayPoint():
     def __init__(self):
         self._amcl_pose = None
-        self.goal_sent = False    
+        self.goal_sent = False   
+
+        self.poseStampedArray = []
+        self.paths = []
+        self.combined_path = Path()
+        self.combined_path.header.frame_id = "map" 
+
+        self.targets = generate_pointstamped()
         
         self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         rospy.loginfo("Wait for the action server to come up")
@@ -29,27 +46,74 @@ class WayPoint():
         
         self.current_pose = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.Pose_callback)
 
-        execute_action_full_name = '/move_base/make_plan'
-        rospy.wait_for_service(execute_action_full_name)
-        self.execute_action = rospy.ServiceProxy(execute_action_full_name, GetPlan)
+        planPath_full_name = '/move_base/make_plan'
+        rospy.wait_for_service(planPath_full_name)
+        self.planPath = rospy.ServiceProxy(planPath_full_name, GetPlan)
 
-        self.mainPlan = rospy.Publisher('/customPlan', Path, queue_size=1)
+        self.mainPlan = rospy.Publisher('customPlan', Path, queue_size=1)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    
+    def create_waypoints(self):
+        
+        start = PoseStamped()
+        start.pose = self.getAmcl_Pose().pose.pose
+        self.poseStampedArray.append(start)
+
+        for point in self.targets:
+
+            quat = Quaternion()
+            quat.w = 1 
+
+            path = Path()
+            path.header.frame_id = 'map'
+            path.header.stamp = rospy.Time.now()
+
+            posestamped = PoseStamped()
+            posestamped.header.frame_id = 'map'
+            posestamped.header.stamp = rospy.Time.now()          
+            posestamped.pose = Pose(point, quat)
+            self.poseStampedArray.append(posestamped)
+
+        self.create_paths_from_Waypoints(self.poseStampedArray)       
+
+    def create_paths_from_Waypoints(self, waypoints):
+
+        for i in range(1, len(waypoints)):
+            start = waypoints[i -1]
+            goal = waypoints[i]
+            tolerance = 0.1
+            sub_path =  self.planPath(start, goal, tolerance)
+            
+            self.path_append(sub_path.plan)
+
+    def path_append(self, path):
+        self.paths.append(path)
+        self.combine_paths()
+
+    def combine_paths(self):
+        self.combined_path.poses = []
+        for path in self.paths:
+            self.combined_path.poses += path.poses
+
+        self.mainPlan.publish(self.combined_path)
 
     def path_callback(self, msg):
         # Loop through each point in the Path message and move the robot to that point
-        for pose in msg.plan.poses:
+        for pose in msg.poses:
             # Get the x and y coordinates of the goal position
             x_goal = pose.pose.position.x
             y_goal = pose.pose.position.y
 
             # Calculate the distance and angle to the goal point
-            current_pose = rospy.wait_for_message('/odom', Odometry).pose.pose
+            current_pose = rospy.wait_for_message("/amcl_pose", PoseWithCovarianceStamped).pose.pose
             x_current = current_pose.position.x
             y_current = current_pose.position.y
 
             # Calculate the desired angle to turn towards the goal
             angle_to_goal = atan2(y_goal - y_current, x_goal - x_current)
+
+            print("angle: " + str(angle_to_goal))
 
             # Create a Twist message to rotate the Turtlebot
             rotate_twist = Twist()
@@ -67,14 +131,13 @@ class WayPoint():
 
             # Wait for the Turtlebot to reach the goal position
             while abs(x_goal - x_current) > 0.1 or abs(y_goal - y_current) > 0.1:
-                current_pose = rospy.wait_for_message('/odom', Odometry).pose.pose
+                current_pose = rospy.wait_for_message("/amcl_pose", PoseWithCovarianceStamped).pose.pose
                 x_current = current_pose.position.x
                 y_current = current_pose.position.y
 
         # Stop the Turtlebot
         stop_twist = Twist()
         self.cmd_vel_pub.publish(stop_twist)
-
 
     def getPlan(self):
 
@@ -88,7 +151,7 @@ class WayPoint():
         tolerance = 0.0
 
         # return plan.goal
-        return self.execute_action(start, goal, tolerance)   
+        return self.planPath(start, goal, tolerance)   
 
     def Pose_callback(self, data):
         self._amcl_pose = data.pose.pose
@@ -116,7 +179,10 @@ if __name__ == "__main__":
 
             elif keyboard.read_key() == 'b':
                 path = navigator.getPlan()
-                navigator.path_callback(path)
+                navigator.path_callback(navigator.combined_path)
+
+            elif keyboard.read_key() == 'p':
+                navigator.create_waypoints()
       
     except rospy.ROSInterruptException:
         rospy.loginfo("Quitting")
